4c6e4a6e70d07b345f4f585b62eece8c91ebe6e1
HediHammami <hedihammami95@gmail.com>
2022-09-11 00:16:38 +0100

    first commit

---
+++ pages/api/stripe.js
@@ -0,0 +1,59 @@
+import Stripe from "stripe";
+
+const stripe = new Stripe(
+  "sk_test_51KRdRTAQu76lYFIK5NUF9ThVH0d9F9R5kezRf8ZU22hB8iUhe1vVjsyQGmfXtb6j5YObaxm2PLetUdzu4K9AHbBo00GFXfIjVE"
+);
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      const params = {
+        submit_type: "pay",
+        mode: "payment",
+        payment_method_types: ["card"],
+        billing_address_collection: "auto",
+        shipping_options: [
+          { shipping_rate: "shr_1LeSWvAQu76lYFIKcf1uaqqs" },
+          { shipping_rate: "shr_1LeSY8AQu76lYFIKHMNWzDA2" },
+        ],
+        line_items: req.body.map((item) => {
+          const img = item.image[0].asset._ref;
+          const newImage = img
+            .replace(
+              "image-",
+              "https://cdn.sanity.io/images/9msw06wn/production/"
+            )
+            .replace("-webp", ".webp");
+
+          return {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: item.name,
+                images: [newImage],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
+            },
+            quantity: item.quantity,
+          };
+        }),
+        success_url: `${req.headers.origin}/success`,
+        cancel_url: `${req.headers.origin}/canceled`,
+      };
+
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create(params);
+
+      res.status(200).json(session);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}
